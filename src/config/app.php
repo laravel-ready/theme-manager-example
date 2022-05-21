b910d6e5e70a6438e5b6caba9485c61d06b505af
Egoist <epsilonelittr@gmail.com>
2022-05-21 21:21:38 +0300

    Remove local imports and paths

--- src/config/app.php
+++ src/config/app.php
@@ -166,6 +166,8 @@ return [
          * Package Service Providers...
          */
 
+        LaravelReady\ThemeManager\ThemeServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
--- src/themes/vendor-x/red-swan/tailwind.config.js
+++ src/themes/vendor-x/red-swan/tailwind.config.js
@@ -4,6 +4,11 @@ module.exports = {
 	// 		'./views/**/*.blade.php',
 	// 	],
     // },
+    safelist: [
+        'bg-red-500',
+        'text-3xl',
+        'lg:text-4xl',
+      ],
     darkMode: 'media', // or 'media' or 'class'
     theme: {
         extend: {},
